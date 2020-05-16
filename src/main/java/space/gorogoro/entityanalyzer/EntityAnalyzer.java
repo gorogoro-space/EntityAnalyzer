550defad49dfca03e9d8a80ee7cd074572b0e3ee
Stnsllet <stnsllet.ml@gmail.com>
2020-05-16 16:11:45 +0900

    add eanear ealimitset command
    limit設定のコマンドと実行すると即自分の近くのチャンクをチェックするシステムを追加

--- src/main/java/space/gorogoro/entityanalyzer/EntityAnalyzer.java
+++ src/main/java/space/gorogoro/entityanalyzer/EntityAnalyzer.java
@@ -271,10 +271,73 @@ public class EntityAnalyzer extends JavaPlugin {
         }
       }
       else if( command.getName().equals("ealimits")) {
-    	  FileConfiguration config = getConfig();
-    	  String le = config.getString("limit_entity");
-    	  String dcr = config.getString("detect_chunk_range");
-    	  EntityAnalyzerUtility.sendMessage(sender, "Limit Entity: " + le + " Detect Chunk Range: " + dcr);
+          FileConfiguration config = getConfig();
+          String le = config.getString("limit_entity");
+          String dcr = config.getString("detect_chunk_range");
+          EntityAnalyzerUtility.sendMessage(sender, "Limit Entity: " + le + " Detect Chunk Range: " + dcr);
+      }
+      else if( command.getName().equals("eanear")) {
+          FileConfiguration config = getConfig();
+          Integer limit_entity = config.getInt("limit_entity");
+          Integer detectChunkRange = config.getInt("detect_chunk_range");
+          Player p = (Player) sender;
+          for(Chunk c: p.getWorld().getLoadedChunks()) {
+	          if(c.getTileEntities().length < limit_entity && c.getEntities().length < limit_entity) {
+	            continue;
+	          }
+
+	          if(getDistance(p.getLocation().getChunk().getX(), p.getLocation().getChunk().getZ(), c.getX(), c.getZ()) > detectChunkRange) {
+	            continue;
+	          }
+
+	            String title = "注意";
+	            String subtitle = "多数のエンティティーを検知しました。(x:"+(c.getX() * 16)+",z:"+(c.getZ() * 16)+")";
+	            p.sendTitle(title, subtitle, 10, 300, 20);
+	            String msg = " " + (detectChunkRange * 16) + "ブロック以内にエンティティーまたはタイルエンティティーが"+limit_entity+"以上のチャンクあります。ホッパー、チェスト、額縁等を分散するか整理をお願いします(x:"+(c.getX() * 16)+",z:"+(c.getZ() * 16)+")";
+	            p.sendMessage(ChatColor.DARK_GRAY + msg);
+	            getLogger().info(msg + " " + p.getName());
+	     }
+      }
+      else if( command.getName().equals("ealimitset")) {
+          Player p = (Player) sender;
+          if(!p.isOp()) {
+        	  EntityAnalyzerUtility.sendMessage(sender, "権限がありません。");
+              return false;
+          }
+          if (args.length != 2){
+              return false;
+          }
+          FileConfiguration config = getConfig();
+          String set_target = String.valueOf(args[0]);
+          if (!set_target.equals("limit") && !set_target.equals("chunk") ) {
+        	  EntityAnalyzerUtility.sendMessage(sender, "First arg is limit or chunk. (" + set_target + ")");
+              return false;
+          }
+          Integer limit = Integer.parseInt(args[1]);
+          if (set_target.equals("limit")) {
+              Integer upper_limit = config.getInt("lower_limit_entity");
+              // 小さすぎる値を排除。過剰反応防止の為
+              if (limit < upper_limit) {
+            	  EntityAnalyzerUtility.sendMessage(sender, "Second arg over upper limit! limit is : "+ String.valueOf(upper_limit));
+                  return false;
+              }
+              EntityAnalyzerUtility.sendMessage(sender, "Update Limit Entity. -> (" + limit + ")");
+              config.set("limit_entity", limit);
+          }
+          else if (set_target.equals("chunk")) {
+        	  Integer upper_limit = config.getInt("upper_limit_detect_chunk_range");
+              // 大きすぎる値を排除。過剰探索防止の為
+              if (limit > upper_limit) {
+            	  EntityAnalyzerUtility.sendMessage(sender,"Second arg over upper limit! limit is : "+ String.valueOf(upper_limit));
+                  return false;
+              }
+              EntityAnalyzerUtility.sendMessage(sender, "Update Detect Chunk Range. -> (" + limit + ")");
+              config.set("detect_chunk_range", limit);
+          }
+
+          // コンフィグリロード
+          saveConfig();
+          reloadConfig();
       }
       return true;
     }catch(Exception e){
